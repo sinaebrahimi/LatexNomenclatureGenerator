03ed18b64ff86fa3c89fb844f8b9971fb1e61a75
Sina Ebrahimi <3bin3m@gmail.com>
2023-05-01 03:16:13 +0100

    More readable code with smaller functions.
    It is tested on my lit. review

--- main.py
+++ main.py
@@ -1,43 +1,50 @@
 import re
 from pathlib import Path
+from typing import List, Dict
 
+def read_file(file: Path) -> str:
+    with open(file, 'r', encoding='utf-8') as f:
+        return f.read()
 
-def generate_acronyms_table(main_tex_file: str) -> str:
-    main_tex_file = Path(main_tex_file)
-    with open(main_tex_file, 'r') as f:
-        main_text = f.read()
-
-    # Find all section files included in the main file
+def find_section_files(main_tex_file: Path) -> List[Path]:
+    main_text = read_file(main_tex_file)
     section_files = re.findall(r'\\input\{(.+?)\}', main_text)
+    return [main_tex_file.parent / section_file for section_file in section_files]
 
-    # Read the content of all section files
+def read_sections_text(section_files: List[Path]) -> str:
     sections_text = ''
     for section_file in section_files:
-        section_file = main_tex_file.parent / section_file
-        with open(section_file, 'r', encoding='utf-8') as f: #with open(section_file, 'r') as f:
-            sections_text += f.read()
+        sections_text += read_file(section_file)
+    return sections_text
 
-    # Find all acronyms in the text
-    acronyms = re.findall(r'\(([A-Z]+)\)', sections_text)
+def find_acronyms(sections_text: str) -> List[str]:
+    return re.findall(r'\(([A-Z]+)\)', sections_text)
 
-    # Find the full description for each acronym
+def find_full_descriptions(acronyms: List[str], sections_text: str) -> Dict[str, str]:
     full_descriptions = {}
     for acronym in acronyms:
-        # Search for the full description before the acronym
         words = [f'([A-Z][a-z]*)' for _ in acronym]
         pattern = rf'{" ".join(words)} \({acronym}\)'
         match = re.search(pattern, sections_text)
         if match:
             full_descriptions[acronym] = ' '.join(match.groups())
+    return full_descriptions
 
-    # Generate the LaTeX table
+def generate_latex_table(full_descriptions: Dict[str, str]) -> str:
     table = '\\begin{table}\n\\begin{tabular}{l|l}\n'
     table += 'Acronym & Full Description\\\\ \\hline\n'
     for acronym, desc in sorted(full_descriptions.items()):
         table += f'{acronym} & {desc}\\\\\n'
     table += '\\hline\n\\end{tabular}\n\\end{table}'
-
     return table
+
+def generate_acronyms_table(main_tex_file: str) -> str:
+    main_tex_file = Path(main_tex_file)
+    section_files = find_section_files(main_tex_file)
+    sections_text = read_sections_text(section_files)
+    acronyms = find_acronyms(sections_text)
+    full_descriptions = find_full_descriptions(acronyms, sections_text)
+    return generate_latex_table(full_descriptions)
 
 
 if __name__ == '__main__':
